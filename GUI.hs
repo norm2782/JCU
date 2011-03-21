2f4fd39e6c8366fc209e01e5e4ea29f63a08a564
3555003 <3555003@46e4088a-c2b5-4d24-90db-838aeec2e325>
2011-03-21 17:49:03 +0000

    Initial GUI setup
    git-svn-id: https://svn.science.uu.nl/repos/edu.3555003.wiskunded/trunk@7 46e4088a-c2b5-4d24-90db-838aeec2e325

--- GUI.hs
+++ GUI.hs
@@ -1,5 +1,9 @@
+module Main where
+
+import Data.Char (isUpper)
+import Data.List (intercalate)
 import Graphics.UI.WX
-import Graphics.UI.WXCore 
+import Prolog
 
 main :: IO ()
 main = start gui
@@ -17,9 +21,9 @@ gui = do -- Application frame
                                 ,  on command ::= onClear rules query output
                                 ]
     set frame  [  layout := column 5  [ boxed "" (grid 5 5 [
-                                             [label "Rules:",  hfill $ widget rules]
-                                          ,  [label "Query:",  hfill $ widget query]
-                                          ,  [label "Output:", hfill $ widget output]
+                                             [label "Rules:",   hfill $ widget rules]
+                                          ,  [label "Query:",   hfill $ widget query]
+                                          ,  [label "Output:",  hfill $ widget output]
                                           ,  [widget run]
                                           ,  [widget clear]
                                           ])
@@ -27,14 +31,40 @@ gui = do -- Application frame
                ,  clientSize := sz 800 600
                ]
 
-onRun rules query output b = do
+onRun :: (Textual a, Textual b, Textual c) => a -> b -> c -> d -> IO ()
+onRun rules query output _ = do
+    set output [ text := "Running..." ]
     rs  <- get  rules   text
-    qs  <- get  query   text
-    os  <- get  output  text
-    return ()
+    let (rules, rerr) = startParse pRules rs
+    if null rerr
+        then do qs  <- get  query   text
+                let (goal, ferr) = startParse pFun qs
+                if null ferr
+                    then do append output "Done!"
+                            showSolutions output $ solve rules [goal] [] 0
+                    else append output $ "Invalid query: " ++ qs
+        else append output $ "Errors in parsing rules!" ++ show rerr
 
-onClear rules query output b = do
+append :: Textual a => a -> String -> IO ()
+append t s = do
+    txt <- get t text
+    set t [ text := '\n' : txt ++ s ]
+
+onClear :: (Textual a, Textual b, Textual c) => a -> b -> c -> d -> IO ()
+onClear rules query output _ = do
     set rules   [ text := "" ]
     set query   [ text := "" ]
     set output  [ text := "" ]
-    return ()
+
+showSolutions :: Textual a => a -> [EnvTrace] -> IO ()
+showSolutions t es = sequence_ [ showSolution t bs | bs <- es]
+
+showSolution :: Textual a => a -> EnvTrace -> IO ()
+showSolution t (bs, trace) = do  mapM_ (append t) trace
+                                 append t $ concatMap showBdg bs
+ where  showBdg (x, t)  |  isUpper (head x) && length x == 1 =  x ++ " = " ++ showTerm t ++ "\n"
+                        |  otherwise = ""  
+        showTerm (Con n)     = show n 
+        showTerm t@(Var _)   = showTerm (lookUp t bs) 
+        showTerm (Fun f [])  = f 
+        showTerm (Fun f ts)  = f ++ "(" ++ intercalate ", " (map showTerm ts) ++ ")"
