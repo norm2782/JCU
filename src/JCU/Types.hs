2a7365bee76121ebc10da190ca03ddcf1d4a2fdc
3555003 <3555003@46e4088a-c2b5-4d24-90db-838aeec2e325>
2011-04-09 18:13:08 +0000

    Separate code and put in separate namespace
    git-svn-id: https://svn.science.uu.nl/repos/edu.3555003.wiskunded/trunk@67 46e4088a-c2b5-4d24-90db-838aeec2e325

---
+++ src/JCU/Types.hs
@@ -0,0 +1,69 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module JCU.Types where
+
+import Data.List (intercalate)
+import Data.Aeson
+
+type Ident  =  String
+
+data Term   =  Con Int
+            |  Var Ident
+            |  Fun Ident [Term]
+            deriving Eq
+
+data Rule   =  Term   :<-: [Term]
+data Trace  =  Trace  { goal   :: Term
+                      , unif   :: Rule
+                      , env    :: Env
+                      , terms  :: [Term] }
+
+type Env       = [(Ident, Term)]
+type EnvTrace  = (Env, [Trace])
+
+instance Show Term where
+  show (Con  i)     = show  i
+  show (Var  i)     =       i
+  show (Fun  i [])  =       i
+  show (Fun  i ts)  = i ++ "(" ++ showCommas ts ++ ")"
+
+instance Show Rule where
+  show (t :<-: []) = show t ++ "."
+  show (t :<-: ts) = show t ++ ":-" ++ showCommas ts ++ "."
+
+instance Show Trace where
+  show (Trace t r e ts) =  display "goal                  : " t   ++
+                           display "unifies with head of  : " r   ++
+                           display "new environment       : " e   ++
+                           display "new goals             : " ts  ++ "\n"
+                      where display str val = str ++ show val ++ "\n"
+
+showCommas :: Show a => [a] -> String
+showCommas l = intercalate ", " (map show l)
+
+class Taggable a where
+  tag :: Int -> a -> a
+
+instance Taggable Term where
+  tag n (Con  x)     = Con  x
+  tag n (Var  x)     = Var  (x ++ show n)
+  tag n (Fun  x xs)  = Fun  x (map (tag n) xs)
+
+instance Taggable Rule where
+  tag n (c :<-: cs) = tag n c :<-: map (tag n) cs
+
+instance ToJSON Term where
+  toJSON (Con number)       = object  [  "number"  .= number]
+  toJSON (Var ident)        = object  [  "ident"   .= ident]
+  toJSON (Fun ident terms)  = object  [  "ident"   .= ident
+                                      ,  "terms"   .= map toJSON terms ]
+
+instance ToJSON Rule where
+  toJSON (term :<-: terms) = object  [  "term"   .= toJSON term
+                                     ,  "terms"  .= map toJSON terms ]
+
+instance ToJSON Trace where
+  toJSON (Trace g u e ts) = object  [  "goal"   .= toJSON g
+                                    ,  "unif"   .= toJSON u
+                                    ,  "env"    .= toJSON e
+                                    ,  "terms"  .= map toJSON ts ]
