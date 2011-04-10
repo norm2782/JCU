f1a06bd8528ae2fcf6745772e62820005126b6ee
Jurriën Stutterheim <j.stutterheim@me.com>
2011-04-10 19:54:40 +0000

    Minro formattign

--- trunk/src/JCU/Prolog/Prolog.hs
+++ trunk/src/JCU/Prolog/Prolog.hs
@@ -13,15 +13,14 @@ lookUp  t        _   =  t
 
 unify :: (Term, Term) -> Maybe Env -> Maybe Env
 unify _       Nothing       = Nothing
-unify (t, u)  env@(Just e)  = trace  ("unifying: " ++ show t ++ " " ++ show u ++ "\n")
-                                     (uni (lookUp t e) (lookUp u e))
-  where  uni (Var x) y          =  Just ((x, y): e)
-         uni x (Var y)          =  Just ((y, x): e)
-         uni (Con x) (Con y)    =  if x == y then env else Nothing
+unify (t, u)  env@(Just e)  = uni (lookUp t e) (lookUp u e)
+  where  uni (Var x) y        =  Just ((x, y): e)
+         uni x (Var y)        =  Just ((y, x): e)
+         uni (Con x) (Con y)  =  if x == y then env else Nothing
          uni (Fun x xs) (Fun y ys)
            | x == y && length xs == length ys  = foldr unify env (zip xs ys)
            | otherwise                         = Nothing
-         uni _ _                =  Nothing
+         uni _ _              =  Nothing
 
 solve :: [Rule] -> [Term] -> Env -> Int -> [EnvTrace]
 solve rules []      e _  = [(e, [])]
@@ -34,19 +33,21 @@ solve rules (t:ts)  e n  =
     ]
 
 -- Solving individual rules:
--- We need the right-hand side of the rule, since the left-hand side would bring
--- us back to where we were.
--- In order to solve the right-hand side, we need the environment with variables
--- which map terms like X0 and Y0 to concrete values.
+-- We need the right-hand side of the rule, since the left-hand side would
+-- bring us back to where we were.
+-- In order to solve the right-hand side, we need the environment with
+-- variables which map terms like X0 and Y0 to concrete values.
 -- Actually, we probably don't even want to show X0 and Y0, but concrete values
 -- instead?
 -- We need to take the environments into account.
 
 showBdg :: Env -> (Ident, Term) -> String
-showBdg bs (x, t)  | isUpper (head x) && length x == 1  = x ++ " = " ++ showTerm t ++ "\n"
-                   | otherwise                          = ""
-  where showTerm :: Term -> String
-        showTerm (Con n)     = show n
-        showTerm t@(Var _)   = showTerm (lookUp t bs) 
-        showTerm (Fun f [])  = f
-        showTerm (Fun f ts)  = f ++ "(" ++ intercalate ", " (map showTerm ts) ++ ")"
+showBdg bs (x, t)  | isShowable  = x ++ " = " ++ showTerm t ++ "\n"
+                   | otherwise   = ""
+  where  isShowable = isUpper (head x) && length x == 1
+         showTerm :: Term -> String
+         showTerm (Con n)     = show n
+         showTerm t@(Var _)   = showTerm (lookUp t bs) 
+         showTerm (Fun f [])  = f
+         showTerm (Fun f ts)  = f ++ "(" ++ intercalate ", " (map showTerm ts)
+                                  ++ ")"
