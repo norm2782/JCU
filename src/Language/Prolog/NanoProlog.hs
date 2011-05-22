0f970d5c55a85a0039987b35dc6ec2de60b2df9c
swier101 <swier101@46e4088a-c2b5-4d24-90db-838aeec2e325>
2011-05-22 14:18:57 +0000

    cabal file
    git-svn-id: https://svn.science.uu.nl/repos/edu.3555003.wiskunded/trunk@195 46e4088a-c2b5-4d24-90db-838aeec2e325

--- src/Language/Prolog/NanoProlog.hs
+++ src/Language/Prolog/NanoProlog.hs
@@ -4,7 +4,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverlappingInstances #-}
 
-module Language.Prolog.NanoProlog where
+module Language.Prolog.NanoProlog (Term(..),Rule((:<-:)),LowerCase,unify,subst,startParse,pRule,pTerm) where
 
 import            Data.Maybe
 import            Data.ListLike.Base (ListLike)
@@ -15,7 +15,7 @@ import            Text.ParserCombinators.UU.Utils
 import            System.IO
 
 
--- * Basic Data Types
+-- * Types
 type UpperCase  = String
 type LowerCase  = String
 
@@ -70,23 +70,24 @@ solve rules  e  n  (t:ts)
                  | rule@(c :<-: cs)   <- tag n rules 
                  ]
 
-
--- * Running the Interpreter, reading data base and printing solutions
+-- * Running the Interpreter
+-- ** The main interpreter
+-- | The `main` program prompt for a file with Prolog rules and call the main interpreter loop
 main :: IO ()
-main =
- do  hSetBuffering stdin LineBuffering
-     putStr "File with rules? "
-     fn  <- getLine
-     s   <- readFile fn
-     let (rules, errors) = startParse (pList pRule)  s
-     if Prelude.null errors then  do  mapM_ print rules
-                                      loop rules
-                            else  do  putStrLn "No rules parsed"
-                                      mapM_ print errors
-                                      main
+main = do  hSetBuffering stdin LineBuffering
+           putStr "File with rules? "
+           fn  <- getLine
+           s   <- readFile fn
+           let (rules, errors) = startParse (pList pRule)  s
+           if Prelude.null errors then  do  mapM_ print rules
+                                            loop rules
+                                  else  do  putStrLn "No rules parsed"
+                                            mapM_ print errors
+                                            main
 
+-- | `loop` ask for a goal, and enuartes all solutions found, each preceded by a trace conatining the rules applied in a tree-like fashion
 loop :: [Rule] -> IO ()
-loop rules = do  putStr "goals? "
+loop rules = do  putStr "goal? "
                  s <- getLine
                  unless (s == "quit") $
                    do  let (goal, errors) = startParse  pFun s
@@ -97,19 +98,22 @@ loop rules = do  putStr "goals? "
                        loop rules
 
 -- ** Printing the solutions
-
+-- | `printSolutions` performs a depth-first walk over the `Result` tree, while accumulating the rules that were applied on the path which was traversed from the root to the current node. At a successful leaf tis contains the full proof
 printSolutions :: IO () -> [String] -> Result -> IO ()
 printSolutions prProof _ (Done env)     = do prProof
+                                             print "solution: "
                                              printEnv env
                                              getLine
                                              return ()
-printSolutions _ _ None             = return ()
+printSolutions _       _ None           = return ()
 printSolutions prProof (pr:prefixes) (ApplyRules  bs) 
     = sequence_ [ printSolutions (prProof >> putStrLn  (pr ++ " " ++ show rule))  (extraPrefixes++prefixes) result 
                 | (rule@(c :<-: cs), result) <-  bs
                 , let extraPrefixes = take (length cs) (map (\i -> pr ++ "." ++ show i) [1..])
                 ]
 
+-- | `printEnv` prints a single solution, shwoing only the variables that were introduced in the original goal
+printEnv :: Env -> IO ()
 printEnv  bs =  putStr (intercalate ", " . filter (not.null) . map  showBdg $ bs)
              where  showBdg (    x,t)  | isGlobVar x =  x ++ " <- "++ showTerm t 
                                        | otherwise = ""   
