03ae1e06b34081c414d4f2b9897bcf5ee5cb40a8
3555003 <3555003@46e4088a-c2b5-4d24-90db-838aeec2e325>
2011-05-22 19:41:36 +0000

    Add uglify
    git-svn-id: https://svn.science.uu.nl/repos/edu.3555003.wiskunded/trunk@207 46e4088a-c2b5-4d24-90db-838aeec2e325

---
+++ uglify.sh
@@ -0,0 +1,3 @@
+uglifyjs ./resources/static/brunch/build/web/js/app.js > tmp
+rm ./resources/static/brunch/build/web/js/app.js
+mv tmp ./resources/static/brunch/build/web/js/app.js
--- upgrade.sh
+++ upgrade.sh
@@ -4,3 +4,4 @@ git pull origin master
 brunch build ./resources/static/brunch
 make deps
 cabal install --disable-documentation
+./uglify.sh
