43e352484ef12b30714d68567e9f5ffed7b4a116
Jurriën Stutterheim <j.stutterheim@me.com>
2012-01-05 21:58:48 +0100

    Add Data.Map

---
+++ resources/static/hjs/Data/Map.hs
@@ -0,0 +1,14 @@
+module Data.Map where
+
+type Map k a = [(k, a)]
+
+lookup :: Eq k => k -> Map k a -> Maybe a
+lookup _ []            = Nothing
+lookup k ((k', a):xs)  | k == k'    = Just a
+                       | otherwise  = lookup k xs
+
+insert :: k -> a -> Map k a -> Map k a
+insert k a = (:) (k, a)
+
+assocs :: Map k a -> [(k, a)]
+assocs = id
