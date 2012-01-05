f12f281efb1361a977f4516d584c0a021dbe3f64
Alessandro Vermeulen <github@spockz.nl>
2012-01-05 21:55:27 +0100

    Missing Data.Map file / implementation

---
+++ resources/static/hjs/Data/Map.hs
@@ -0,0 +1,16 @@
+module Data.Map where
+  
+type Map k v = [(k,v)]
+
+empty :: Map k v
+empty = []
+
+lookup :: Eq k => k -> Map k v -> Maybe v
+lookup _ [] = Nothing
+lookup k ((k', v):xs) | k == k'   = Just v
+                      | otherwise = lookup k xs
+
+insert :: k -> v -> Map k v -> Map k v
+insert k v = (:) (k,v)
+
+assocs = id
